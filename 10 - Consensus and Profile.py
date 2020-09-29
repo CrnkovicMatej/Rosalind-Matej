1f9c7f2001b5c9aeeb0da0253120362fd61e5c5e
CrnkovicMatej <61793769+CrnkovicMatej@users.noreply.github.com>
2020-09-29 13:54:08 +0200

    Update and rename Consensus and Profile.py to 10 - Consensus and Profile.py

--- 10 - Consensus and Profile.py
+++ 10 - Consensus and Profile.py
@@ -1,9 +1,8 @@
-##napomena: kada preuzmes datot,eku ne bude pravilna raspodijela slova po redcima
+##napomena: kada preuzmes datoteku, ne bude pravilna raspodijela slova po redcima
 ##pa na kraju od svega treba obrisati onoliko slova/brojeva koliko fali u zadnjem retku pojedinog DNa stringa
 
 f1=open("rosalind_cons.txt","r")
 ak=f1.read().split("\n")
-##ak=f1.read().split(">") 
 f1.close()
 onlyDnaStrings = []
 oneDNa = []
@@ -11,7 +10,6 @@ howBigIsOnlyDNa=-1
 where_I_am=0
 while (where_I_am<len(ak)):
         if(ak[where_I_am][0]==">"):
-##            print("jesam te", ak[where_I_am][10] )
             howBigIsOnlyDNa+=1
             where_I_am+=1
             if(howBigIsOnlyDNa>0):
@@ -23,22 +21,18 @@ while (where_I_am<len(ak)):
             onlyDnaStrings.append(oneDNa)
             break
 consensus=""
-##print(onlyDnaStrings)
-##for i in range (len(onlyDnaStrings)):
-##    onlyDnaStrings[i]=onlyDnaStrings[i].join(",")
-##print(onlyDnaStrings)
 matrica = [[0]*(len(onlyDnaStrings[0])*len(onlyDnaStrings[0][0])) for i in range (4)]
 for i in range(len(onlyDnaStrings)):
     for j in range(len(onlyDnaStrings[i])):
         for k in range(len(onlyDnaStrings[i][j])):
            if(onlyDnaStrings[i][j][k]=="A"):
-            matrica[0][j*len(onlyDnaStrings[0][0])+k]+=1
+                matrica[0][j*len(onlyDnaStrings[0][0])+k]+=1
            if(onlyDnaStrings[i][j][k]=="C"):
-            matrica[1][j*len(onlyDnaStrings[0][0])+k]+=1
+                matrica[1][j*len(onlyDnaStrings[0][0])+k]+=1
            if(onlyDnaStrings[i][j][k]=="G"):
-            matrica[2][j*len(onlyDnaStrings[0][0])+k]+=1
+                matrica[2][j*len(onlyDnaStrings[0][0])+k]+=1
            if(onlyDnaStrings[i][j][k]=="T"):
-            matrica[3][j*len(onlyDnaStrings[0][0])+k]+=1
+                matrica[3][j*len(onlyDnaStrings[0][0])+k]+=1
 simbols=["A","C","G","T"]
 for j in range (len(matrica[0])):
     maxbr=0
