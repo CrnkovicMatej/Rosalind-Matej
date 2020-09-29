845a01ffa6aaa4d709fd43e4f6050cbaafe2aec9
CrnkovicMatej <61793769+CrnkovicMatej@users.noreply.github.com>
2020-09-29 13:36:52 +0200

    Update Translating RNA into Protein.py

--- Translating RNA into Protein.py
+++ Translating RNA into Protein.py
@@ -6,16 +6,13 @@ for i in range (len(cod)):
     cod[i]=cod[i].split(" ")
     while(("") in cod[i]):
         cod[i].remove("")
-    #print(cod[i])
 
 rnaString=""
 protein=""
 f2=open("rosalind_prot.txt", "r")
 bla=f2.read().replace(" ","")
 f2.close()
-#print(bla[9049])
-#print(len(bla))
-##print(cod[2][0])
+
 for i in range (1,(len(bla)+1)):
     if((i%3)==0):
         rnaString+=bla[i-1]
