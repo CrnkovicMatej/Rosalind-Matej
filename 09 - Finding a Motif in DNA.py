4c3a1b834e21361a04d03c76d036aacf26e81f6d
CrnkovicMatej <61793769+CrnkovicMatej@users.noreply.github.com>
2020-09-29 13:51:16 +0200

    Update and rename Finding a Motif in DNA.py to 09 - Finding a Motif in DNA.py

--- 09 - Finding a Motif in DNA.py
+++ 09 - Finding a Motif in DNA.py
@@ -2,8 +2,7 @@ f2=open("rosalind_subs.txt", "r")
 s=f2.readline().replace("\n","")
 t=f2.readline().replace("\n","")
 f2.close()
-##print(s)
-##print(t)
+
 list=[]
 for i in range(len(s)):
     if(s[i:i+len(t)]==t):
