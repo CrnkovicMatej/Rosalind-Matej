546e9b2b2b5983d379b9a592a0de21f727977815
CrnkovicMatej <61793769+CrnkovicMatej@users.noreply.github.com>
2020-09-29 13:46:14 +0200

    Update and rename Complementing a Strand of DNA.py to 03 - Complementing a Strand of DNA.py

--- 03 - Complementing a Strand of DNA.py
+++ 03 - Complementing a Strand of DNA.py
@@ -1,11 +1,9 @@
 f1=open("rosalind_revc.txt","r")
 ak=f1.readline()
 f1.close()
-##ak="AAAACCCGGT"
+
 new = ak[::-1]
-##not a good solution
-##for r in (("A","T"),("T","A"),("C","G"),("G","C")):
-##    new.replace(*r)
+
 for i in range (len(new)):
     if(new[i]=="A"):
         new=new[:i]+'T'+new[i+1:]
